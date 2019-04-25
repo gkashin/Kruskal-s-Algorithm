acdb8518cf3abf6d97c15be06ce623de21789aab
George <georgii.kashin@gmail.com>
2019-04-25 12:40:11 +0300

    Initial Commit

---
+++ Coursework#2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coursework#2
+//
+//  Created by Георгий Кашин on 25/04/2019.
+//  Copyright © 2019 Georgii Kashin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
